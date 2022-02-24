8a5163e488d5057efe0d38de545164f6744566f2
ZoxyModz <74683259+Zoxouu@users.noreply.github.com>
2022-02-24 21:49:47 +0100

    Create NodeJS-12.sh

---
+++ NodeJS-12.sh
@@ -0,0 +1,6 @@
+apt install nodejs npm (V10 par default)
+curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh (remplace le 12 par la version voulu)
+bash nodesource_setup.sh
+apt install nodejs
+node -v
+npm install pm2@latest -g commands
