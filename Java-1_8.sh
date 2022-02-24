17bc8290a4f14745f1b81420700d331a2bfef3f9
ZoxyModz <74683259+Zoxouu@users.noreply.github.com>
2022-02-24 21:47:20 +0100

    Create Java-1_8.sh

---
+++ Java-1_8.sh
@@ -0,0 +1,5 @@
+apt install apt-transport-https ca-certificates wget dirmngr gnupg software-properties-common
+wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | apt-key add -
+add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
+apt-get update && apt-get upgrade -y
+apt install adoptopenjdk-8-hotspot
