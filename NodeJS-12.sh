219e4d8c9bf92c9572f475543c74b116ee351c95
ZoxyModz <74683259+Zoxouu@users.noreply.github.com>
2022-04-25 21:09:20 +0200

    Update NodeJS-12.sh

--- NodeJS-12.sh
+++ NodeJS-12.sh
@@ -1,4 +1,4 @@
-apt install nodejs npm (V10 par default)
+apt install nodejs npm
 curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh (remplace le 12 par la version voulu)
 bash nodesource_setup.sh
 apt install nodejs
